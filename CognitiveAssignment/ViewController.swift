b43d418209975a72972d9d02f6c64bd235b295f3
Shane Jackson <shanejackson@Shanes-MacBook-Air.local>
2017-03-28 18:10:38 -0700

    Initial Commit

---
+++ CognitiveAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CognitiveAssignment
+//
+//  Created by Shane Jackson on 2017-03-28.
+//  Copyright © 2017 Shane Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
