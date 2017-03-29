9c6ba6bf6f915c1e0519ffe39dffae9b189e8fa5
Shane Jackson <shanejackson@Shanes-MacBook-Air.local>
2017-03-28 20:21:52 -0700

    first commit

---
+++ CognitiveAssignment/AnalyzeImageDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AnalyzeImageDelegate.swift
+//  CognitiveAssignment
+//
+//  Created by Shane Jackson on 2017-03-28.
+//  Copyright © 2017 Shane Jackson. All rights reserved.
+//
+
+import Foundation
